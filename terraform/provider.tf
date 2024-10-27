8e895984d96a8874abd8bff420ff1a38f2fe31c6
Kitpivat Piromgit <103819686+shirocola@users.noreply.github.com>
2024-10-27 22:11:29 +0700

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -12,6 +12,6 @@ terraform {
 provider "google" {
   project     = var.project_id
   region      = var.region
-  zone        = "us-central1-a"    # Specify the zone here
-  credentials = file("./devops-assignment-439616-69270a783cda.json")
+  zone        = var.zone
+  credentials = file(var.credentials_file)
 }
